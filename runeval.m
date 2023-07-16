9530a3e1dba46fa03d75d8f0b5cc342411ed46de
Kipngeno Hillary Bett <135506318+chumoyot@users.noreply.github.com>
2023-07-16 11:17:04 +0300

    Add files via upload

---
+++ runeval.m
@@ -0,0 +1,2 @@
+testpath = 'C:\Users\KIPNGENO\Documents\Documents\ROBOTICS\particle filter\AssignmentWEEK4'; % This is the folder where test.mat is saved. Change the path if needed.
+eval_progW4(testpath)
